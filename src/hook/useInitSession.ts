9ab8d351f120599bf6769d91c2b7c1c2e8f87dfa
marco.meloni <marco.meloni@emeal.nttdata.com>
2026-05-18 15:23:06 +0200

    chore: added notify user of tpp not available after redirect on onboarding

--- src/hook/useInitSession.ts
+++ src/hook/useInitSession.ts
@@ -9,11 +9,15 @@ import {
     setTppIdCheckFailed,
 } from '../redux/slices/organizationSlice';
 import { getOrganizationFromStorage } from '../utils/organization';
+import { appStateActions } from '@pagopa/selfcare-common-frontend/lib/redux/slices/appStateSlice';
+import { store } from '../redux/store';
 
 export const useInitSession = () => {
     const dispatch = useAppDispatch();
 
     useEffect(() => {
+        localStorage.removeItem('acs_tpp_id');
+
         const organization = getOrganizationFromStorage();
         const user = storageUserOps.read();
 
@@ -22,7 +26,25 @@ export const useInitSession = () => {
 
         getTppProfile()
             .then((response) => {
-                dispatch(setTppRegistered(response !== null));
+                if (response === null) {
+                    const wasRegistered = localStorage.getItem('tpp_registered') === 'true';
+                    if (wasRegistered) {
+                        localStorage.removeItem('tpp_registered');
+                        store.dispatch(appStateActions.addError({
+                            id: 'TPP_NOT_FOUND',
+                            error: new Error('TPP not found'),
+                            techDescription: 'TPP not found',
+                            blocking: false,
+                            toNotify: true,
+                            component: 'Toast',
+                            displayableDescription: "La TPP non è più disponibile. Ripetere la registrazione o contattare l'assistenza",
+                        }));
+                    }
+                    dispatch(setTppRegistered(false));
+                } else {
+                    localStorage.setItem('tpp_registered', 'true');
+                    dispatch(setTppRegistered(true));
+                }
             })
             .catch(() => {
                 dispatch(setTppIdCheckFailed());
