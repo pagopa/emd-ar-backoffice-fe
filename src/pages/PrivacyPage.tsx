c3569f2f866f4c56487bb8e1444fe610734d7e1a
Marco-Meloni-30 <marco.meloni@emeal.nttdata.com>
2026-09-24 16:56:16 +0200

    removed isssues sonar

--- src/pages/PrivacyPage.tsx
+++ src/pages/PrivacyPage.tsx
@@ -1,7 +1,6 @@
 import OneTrustNotice from "../components/layoutPages/Onetrustnotice";
 import { CONFIG } from "../config";
 import '../assets/css/oneTrustSidebar.css'
-import '../assets/css/privacy.css'
 
 function PrivacyPage() {
     return <OneTrustNotice noticeId={CONFIG.ONE_TRUST.PRIVACY_NOTICE_ID} language={'it'} />
